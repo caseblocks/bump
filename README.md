fe8c474436c5cd5ca7c547e2a6c65b4471799455
Ijonas Kisselbach <ijonas@emergeadapt.com>
2017-11-08 06:55:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# bump
+
+Increases a counter in VERSION files. Output is to STDOUT and the original VERSION file is left unmodified.
+
+# Usage
+
+Assuming a VERSION file containting `1.2.3.` in the current folder:
+
+    bump major          # Output 2.2.3
+
+    bump minor          # Output 1.3.3
+
+    bump patch          # Output 1.2.4
+
+    bump somethingelse  # Output 1.2.3
+
+To modify a VERSION file:
+
+    bump major > VERSION  # overwrites current file
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	bytez, err := ioutil.ReadFile("VERSION")
+	if err != nil {
+		panic(err)
+	}
+
+	currentVersion := strings.TrimSpace(string(bytez))
+
+	parts := strings.Split(currentVersion, ".")
+	major, _ := strconv.Atoi(parts[0])
+	minor, _ := strconv.Atoi(parts[1])
+	patch, _ := strconv.Atoi(parts[2])
+
+	var bump string
+	if len(os.Args) > 1 {
+		bump = os.Args[1]
+	}
+
+	if bump == "major" {
+		major++
+	} else if bump == "minor" {
+		minor++
+	} else if bump == "patch" {
+		patch++
+	}
+
+	fmt.Printf("%d.%d.%d\n", major, minor, patch)
+
+}
